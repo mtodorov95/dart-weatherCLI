47b15995fe862eb8ed70d3c53b71e2075fec3a49
mtodorov95 <mtodorov95@gmail.com>
2020-11-12 17:52:40 +0200

    weather api

---
+++ lib/api.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather/model.dart';
+
+class WeatherApi {
+  static const ID_URL =
+      'https://www.metaweather.com/api/location/search/?query=[0]';
+  static const API_URL = 'https://www.metaweather.com/api/location/[0]/';
+
+  Future<int> getCityId(String city) async {
+    String url = ID_URL.replaceAll('[0]', city);
+    http.Response res = await http.get(url);
+    var json = jsonDecode(res.body);
+    if ((json as List).isEmpty) {
+      throw Exception('Can not find City');
+    }
+    return json[0]['woeid'];
+  }
+
+  Future<List<Weather>> getWeather(String city) async {
+    int id = await getCityId(city);
+    String url = API_URL.replaceAll('[0]', id.toString());
+    http.Response res = await http.get(url);
+    var json = jsonDecode(res.body);
+    List<Weather> weather = [];
+    (json['consolidated_weather'] as List<dynamic>)
+      ..forEach((e) => weather.add(Weather.fromJsom(e)));
+    return weather;
+  }
+}
--- lib/model.dart
+++ lib/model.dart
@@ -5,4 +5,15 @@ class Weather {
   String date;
 
   Weather({this.temp, this.date, this.maxTemp, this.minTemp});
+
+  Weather.fromJsom(Map<String, dynamic> json)
+      : temp = json['the_temp'],
+        minTemp = json['min_temp'],
+        maxTemp = json['max_temp'],
+        date = json['applicable_date'];
+
+  @override
+  String toString() {
+    return 'Date: $date, Temp: ${temp.toStringAsFixed(2)}C, Min Temp: ${minTemp.toStringAsFixed(2)}C, Max Temp: ${maxTemp.toStringAsFixed(2)}C\n';
+  }
 }
